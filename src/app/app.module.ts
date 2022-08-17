6627c534bc9758107bfc012f3ae4d5d65d3a25cc
indracurious <76813143+indrajeetgiram@users.noreply.github.com>
2022-08-17 18:30:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { P1Component } from './p1/p1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    P1Component
   ],
   imports: [
     BrowserModule,
